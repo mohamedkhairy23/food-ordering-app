8bd292b1258d7f92d622f9025a402beb7e805a03
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-06-23 03:22:09 +0300

    Update profile page functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,12 @@ mongoose
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // /api/my/user
